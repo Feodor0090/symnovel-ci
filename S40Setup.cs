5495353cac47e57875a522c3f4adf3a7997209c0
Feodor0090 <79257300125@ya.ru>
2021-09-13 20:28:23 +0300

    Recreate the project as .NET Core

---
+++ S40Setup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace snci
+{
+    class S40Setup
+    {
+        public static void UnpackSDK()
+        {
+
+        }
+    }
+}

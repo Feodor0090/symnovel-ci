acb2f5639c5f7e21c576abcc8bcd742b36b87dd8
Feodor0090 <79257300125@ya.ru>
2021-09-14 14:46:19 +0300

    Fix j2me lib again

--- Program.cs
+++ Program.cs
@@ -23,7 +23,7 @@ namespace symnovel_ci
         static string getJavaBuildParams(string fileName)
         {
             string wd = Environment.CurrentDirectory;
-            return "-encoding UTF-8 -bootclasspath lib -sourcepath "+wd+"\\repo\\src -d bin "+fileName;
+            return "-bootclasspath "+wd+ "\\lib\\lib.jar -encoding UTF-8 -sourcepath " + wd+"\\repo\\src -d bin "+fileName;
         }
 
         static void Build()

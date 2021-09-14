dfcf84f02dfdb89beb93ba5c4f32e19202f83988
Feodor0090 <79257300125@ya.ru>
2021-09-14 14:12:06 +0300

    Skip unpacking to prevent crash

--- EnvSetup.cs
+++ EnvSetup.cs
@@ -17,6 +17,11 @@ namespace symnovel_ci
 
         private static void UnpackZip(string name)
         {
+            if(Directory.Exists(name))
+            {
+                Console.WriteLine(name + " folder already exists, skipping...");
+                return;
+            }
             FileStream stream = File.OpenRead(name+".zip");
             using (ZipArchive classes = new ZipArchive(stream, ZipArchiveMode.Read, false))
             {
